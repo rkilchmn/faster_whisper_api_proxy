c05fe1ddaa04662999d44cb952fc71e60cb35715
rkilchmn <roger@kilchenmann.net>
2024-04-21 19:25:33 +1000

    fixing installation issues

--- setup.py
+++ setup.py
@@ -7,17 +7,13 @@ setup(
     version='1.0.0',    # Version number
     description='a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API',
     packages=['faster_whisper_api_proxy'],  # List of packages to include
-    url='https://github.com/rkilchmn/faster_whisper_api_proxy'
+    url='https://github.com/rkilchmn/faster_whisper_api_proxy',
     author='Roger Kilchenmann',
     author_email='roger@kilchenmann.net',
     keywords=['AI','faster_whisper','transcribe','natural language','speech recognition','api','remote'],
     install_requires=[ 
         'faster_whisper_api_proxy',
-        'typing',
         'numpy',
-        'requests',
-        'json',
-        'collections',
-        'urllib'
+        'requests'
     ],     
 )
