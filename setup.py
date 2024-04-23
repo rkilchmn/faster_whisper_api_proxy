0c458ad5813110f0f9491eee96657dc78edac3a0
rkilchmn <roger@kilchenmann.net>
2024-04-23 19:27:47 +1000

    syntax fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='faster_whisper_api_proxy',  # Name of your module
     version='1.0.1',    # Version number
     description='a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API',
-    packages='faster_whisper_api_proxy',  # find all packages/files where directory contains a __init__.py file
+    packages=['faster_whisper_api_proxy'],  # find all packages/files where directory contains a __init__.py file
     url='https://github.com/rkilchmn/faster_whisper_api_proxy',
     author='Roger Kilchenmann',
     author_email='roger@kilchenmann.net',
