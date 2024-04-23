9114ca5d3662261db5b56e7c17ca71e61889579d
rkilchmn <roger@kilchenmann.net>
2024-04-23 19:06:51 +1000

    use find_package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # setup.py
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='faster_whisper_api_proxy',  # Name of your module
     version='1.0.1',    # Version number
     description='a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API',
-    packages=['faster_whisper_api_proxy'],  # List of packages to include
+    packages=find_packages(),  # List of packages to include
     url='https://github.com/rkilchmn/faster_whisper_api_proxy',
     author='Roger Kilchenmann',
     author_email='roger@kilchenmann.net',
