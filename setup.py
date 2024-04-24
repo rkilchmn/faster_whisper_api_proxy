fabd2097c6214b35b074913aeaec2e54ff717d7a
rkilchmn <roger@kilchenmann.net>
2024-04-24 16:49:33 +1000

    restructured package

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 # setup.py
+import pathlib
+from setuptools import setup
+import pkg_resources
+import os
 
-from setuptools import setup, find_packages
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+def read_version(fname="src/version.py"):
+    exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
+    return locals()["__version__"]
+
 
 setup(
     name='faster_whisper_api_proxy',  # Name of your module
-    version='1.0.1',    # Version number
     description='a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API',
-    packages=['faster_whisper_api_proxy'],  # find all packages/files where directory contains a __init__.py file
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version=read_version(),
     url='https://github.com/rkilchmn/faster_whisper_api_proxy',
     author='Roger Kilchenmann',
     author_email='roger@kilchenmann.net',
     keywords=['AI','faster_whisper','transcribe','natural language','speech recognition','api','remote'],
+    packages=["src"],
+    include_package_data=True,
     install_requires=[
-        'numpy',
-        'requests'
-    ],     
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
 )
